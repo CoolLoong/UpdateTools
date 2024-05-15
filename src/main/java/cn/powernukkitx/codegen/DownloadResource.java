4e8792165804e170226f28dcc436667acabc116e
CoolLoong <1542536763@qq.com>
2024-05-16 02:13:30 +0800

    tmp

--- src/main/java/cn/powernukkitx/codegen/DownloadResource.java
+++ src/main/java/cn/powernukkitx/codegen/DownloadResource.java
@@ -1,6 +1,15 @@
 package cn.powernukkitx.codegen;
 
 import cn.powernukkitx.codegen.util.DownloadUtil;
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Allay Project 12/18/2023
@@ -8,9 +17,9 @@ import cn.powernukkitx.codegen.util.DownloadUtil;
  * @author Cool_Loong
  */
 public class DownloadResource {
-    static String version = "1.20.72.01";
+    static String version = "1.20.81.01";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DownloadUtil.download("https://github.com/CloudburstMC/Data/raw/master/block_palette.nbt",
                 "src/main/resources/block_palette.nbt");
         DownloadUtil.download("https://github.com/CloudburstMC/Data/raw/master/biome_definitions.dat",
@@ -23,5 +32,44 @@ public class DownloadResource {
                 "src/main/resources/creative_items.json");
         DownloadUtil.download("https://github.com/AllayMC/BedrockData/blob/main/%s/block_property_types.json".formatted(version),
                 "src/main/resources/block_property_types.json");
+
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/potion_type.json",
+                "src/main/resources/vanilla_recipes/potion_type.json");
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/shaped_crafting.json",
+                "src/main/resources/vanilla_recipes/shaped_crafting.json");
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/shapeless_crafting.json",
+                "src/main/resources/vanilla_recipes/shapeless_crafting.json");
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/smithing.json",
+                "src/main/resources/vanilla_recipes/smithing.json");
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/shapeless_shulker_box.json",
+                "src/main/resources/vanilla_recipes/shapeless_shulker_box.json");
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/smelting.json",
+                "src/main/resources/vanilla_recipes/smelting.json");
+        DownloadUtil.download("https://github.com/pmmp/BedrockData/raw/master/recipes/special_hardcoded.json",
+                "src/main/resources/vanilla_recipes/special_hardcoded.json");
+
+        updateRecipes();
+    }
+
+    private static void updateRecipes() throws IOException {
+        Path p = Path.of("src/main/resources/vanilla_recipes/shaped_crafting.json");
+        //todo These are wrong recipes, their `result` should not have `data`, if this bug is fixed, please remove it
+        Map<String, String> errorRecipes = Map.of(
+                "minecraft:dispenser", "CgAAAxAAZmFjaW5nX2RpcmVjdGlvbgAAAAABDQB0cmlnZ2VyZWRfYml0AAA=",
+                "minecraft:dropper", "CgAAAxAAZmFjaW5nX2RpcmVjdGlvbgAAAAABDQB0cmlnZ2VyZWRfYml0AAA=",
+                "minecraft:piston", "CgAAAxAAZmFjaW5nX2RpcmVjdGlvbgAAAAAA",
+                "minecraft:sticky_piston", "CgAAAxAAZmFjaW5nX2RpcmVjdGlvbgAAAAAA"
+        );
+        Gson gson = new Gson();
+        List<Map<String, Object>> data = gson.fromJson(new FileReader(p.toFile()), List.class);
+        for (var fix : data) {
+            List<Map<String, Object>> output = (List<Map<String, Object>>) fix.get("output");
+            Map<String, Object> o = output.get(0);
+            var name = o.get("name").toString();
+            if (errorRecipes.containsKey(name)) {
+                o.put("block_states", errorRecipes.get(name));
+            }
+        }
+        Files.writeString(p, gson.toJson(data), StandardCharsets.UTF_8);
     }
 }
--- src/main/java/cn/powernukkitx/codegen/RecipeFixGen.java
+++ src/main/java/cn/powernukkitx/codegen/RecipeFixGen.java
@@ -1,6 +1,10 @@
 package cn.powernukkitx.codegen;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
@@ -11,69 +15,72 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
 public class RecipeFixGen {
-    static Set<String> fixForSlab = Set.of("minecraft:stone_block_slab", "minecraft:stone_block_slab2", "minecraft:stone_block_slab3", "minecraft:stone_block_slab4");
-
-    public static int slabAux(String s) {
-        return switch (s) {
-            case "minecraft:smooth_stone:0", "minecraft:red_sandstone:0", "minecraft:end_stone:0",
-                 "minecraft:end_bricks:0", "minecraft:stonebrick:1", "minecraft:red_sandstone:1" -> 0;
-            case "minecraft:sandstone:0", "minecraft:sandstone:1", "minecraft:purpur_block:0",
-                 "minecraft:red_sandstone:3", "minecraft:quartz_block:3" -> 1;
-            case "minecraft:prismarine:0", "minecraft:polished_andesite:0", "minecraft:stone:0" -> 2;
-            case "minecraft:cobblestone:0", "minecraft:prismarine:1", "minecraft:andesite:0", "minecraft:sandstone:2" ->
-                    3;
-            case "minecraft:brick_block:0", "minecraft:prismarine:2", "minecraft:diorite:0",
-                 "minecraft:red_sandstone:2" -> 4;
-            case "minecraft:stonebrick:0", "minecraft:mossy_cobblestone:0", "minecraft:polished_diorite:0" -> 5;
-            case "minecraft:quartz_block:0", "minecraft:sandstone:3", "minecraft:granite:0" -> 6;
-            case "minecraft:nether_brick:0", "minecraft:red_nether_brick:0", "minecraft:polished_granite:0" -> 7;
-            default -> throw new IllegalStateException("Unexpected value: " + s);
-        };
-    }
+    static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().setNumberToNumberStrategy(JsonReader::nextInt).setObjectToNumberStrategy(JsonReader::nextInt).create();
+    static List<JsonObject> shapelessCrafting;
+    static List<JsonObject> shapedCrafting;
 
     public static void main(String[] args) {
+        loadShapelessCraft();
+        loadShapeCraft();
+
         try (var recipe = RecipeFixGen.class.getClassLoader().getResourceAsStream("recipes.json")) {
-            Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().setNumberToNumberStrategy(JsonReader::nextInt).setObjectToNumberStrategy(JsonReader::nextInt).create();
             Map map = gson.fromJson(new InputStreamReader(recipe), Map.class);
             JsonObject jsonTree = gson.toJsonTree(map).getAsJsonObject();
             JsonArray recipes = jsonTree.getAsJsonArray("recipes");
 
-            //fix for stone_block_slab output
-            for (var member : recipes.asList()) {
-                JsonObject o = member.getAsJsonObject();
-                int type = o.get("type").getAsInt();
-
-                if (type == 0 || type == 1) {//fix slab craft for stonecutter or crafting_table
-                    List<JsonElement> output = o.getAsJsonArray("output").asList();
-                    JsonObject block = output.get(0).getAsJsonObject();
-                    if (fixForSlab.contains(block.get("id").getAsString()) && !block.has("auxValue")) {
-                        JsonObject input;
-                        if (type == 0) {
-                            input = o.getAsJsonArray("input").asList().get(0).getAsJsonObject();
-                        } else {
-                            input = o.getAsJsonObject("input").get("A").getAsJsonObject();
+            //load recipes
+            for (var r : recipes) {
+                JsonObject obj = r.getAsJsonObject();
+                int type = obj.get("type").getAsInt();
+                switch (type) {
+                    case 0, 5, 8 -> {
+                        String block = obj.get("block").toString();
+                        fixShapeLessRecipe(obj, block);
+                    }
+                    case 1 -> {
+                        fixShapeLessRecipe(obj);
+                    }
+                    case 3 -> {
+                        String craftingBlock = (String) recipe.get("block");
+                        Map<String, Object> resultMap = (Map<String, Object>) recipe.get("output");
+                        ItemDescriptor resultItem = parseRecipeItem(resultMap);
+                        if (resultItem == null) {
+                            yield null;
                         }
-                        block.addProperty("auxValue", slabAux(input.get("itemId").getAsString() + ":" + input.get("auxValue").getAsInt()));
-                        output.set(0, block);
+                        Map<String, Object> inputMap = (Map<String, Object>) recipe.get("input");
+                        ItemDescriptor inputItem = parseRecipeItem(inputMap);
+                        if (inputItem == null) {
+                            yield null;
+                        }
+                        Item result = resultItem.toItem();
+                        Item input = inputItem.toItem();
+                        Recipe furnaceRecipe = switch (craftingBlock) {
+                            case "furnace" -> new FurnaceRecipe(result, input);
+                            case "blast_furnace" -> new BlastFurnaceRecipe(result, input);
+                            case "smoker" -> new SmokerRecipe(result, input);
+                            case "campfire" -> new CampfireRecipe(result, input);
+                            case "soul_campfire" -> new SoulCampfireRecipe(result, input);
+                            default -> throw new IllegalStateException("Unexpected value: " + craftingBlock);
+                        };
+                        var xp = furnaceXpConfig.getDouble(input.getId() + ":" + input.getDamage());
+                        if (xp != 0) {
+                            this.setRecipeXp(furnaceRecipe, xp);
+                        }
+                        yield furnaceRecipe;
                     }
+                    default -> throw new IllegalStateException("Unexpected value: " + recipe);
                 }
-
-                if (type == 1) {//fix craft for axe
-                    List<JsonElement> output = o.getAsJsonArray("output").asList();
-                    JsonObject block = output.get(0).getAsJsonObject();
-                    JsonObject in = o.getAsJsonObject("input");
-                    if (block.get("id").getAsString().endsWith("_axe") && in.has("A") && in.has("B")) {
-                        in.addProperty("mirror", true);
+                if (re == null) {
+                    if (type != 9) {//todo trim smithing recipe
+                        log.warn("Load recipe {} with null!", recipe.toString().substring(0, 60));
                     }
+                    continue;
                 }
-
-                fixSandStoneRecipe(o);
-                fixQuartzBlockRecipe(o);
+                this.register(re);
             }
+
             Path path = Path.of("build/recipes.json");
             Files.deleteIfExists(path);
             Files.writeString(path, gson.toJson(jsonTree), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
@@ -82,59 +89,99 @@ public class RecipeFixGen {
         }
     }
 
-    //fix recipe about sandstone
-    public static void fixSandStoneRecipe(JsonObject o) {
-        String id = Optional.ofNullable(o.get("id")).map(JsonElement::getAsString).orElse("");
-        JsonElement output = o.get("output");
-        if (output instanceof JsonArray jsonArray) {
-            List<JsonElement> outputList = jsonArray.asList();
-            JsonObject block = outputList.get(0).getAsJsonObject();
-            if (id.equals("minecraft:stonecutter_sandstone_heiroglyphs") ||
-                    id.equals("heiroglyphs_sandstone_recipeId") ||
-                    id.equals("minecraft:stonecutter_red_sandstone_heiroglyphs") ||
-                    id.equals("heiroglyphs_redsandstone_recipeId")
-            ) {
-                block.addProperty("auxValue", 1);
-                outputList.set(0, block);
-            } else if (id.equals("minecraft:stonecutter_sandstone_cut") || id.equals("minecraft:stonecutter_red_sandstone_cut")) {
-                block.addProperty("auxValue", 2);
-                outputList.set(0, block);
-            } else if (id.equals("minecraft:smooth_sandstone") || id.equals("minecraft:smooth_red_sandstone")) {
-                block.addProperty("auxValue", 3);
-                outputList.set(0, block);
-            }
-        }
-        if (Optional.ofNullable(o.get("block")).map(JsonElement::getAsString).orElse("").equals("furnace")) {
-            JsonObject outputF = o.getAsJsonObject("output");
-            String str = o.getAsJsonObject("output").get("id").getAsString();
-            if (str.equals("minecraft:sandstone")
-                    || str.equals("minecraft:red_sandstone")) {
-                outputF.addProperty("auxValue", 3);
+    static void fixShapeLessRecipe(JsonObject org, String block) {
+        f1:
+        for (var b : shapelessCrafting) {
+            String asString = b.get("block").getAsString();
+            JsonArray a1 = b.get("input").getAsJsonArray();
+            JsonArray a2 = org.get("input").getAsJsonArray();
+            JsonArray o1 = b.get("output").getAsJsonArray();
+            JsonArray o2 = org.get("output").getAsJsonArray();
+            if (asString.equals(block) && a1.size() == a2.size() && o1.size() == o2.size()) {
+                if (setShapeLessAux(a1, a2)) {
+                    continue f1;
+                }
+                if (setShapeLessAux(o1, o2)) {
+                    continue f1;
+                }
             }
         }
     }
 
-    public static void fixQuartzBlockRecipe(JsonObject o) {
-        String id = Optional.ofNullable(o.get("id")).map(JsonElement::getAsString).orElse("");
-        JsonElement output = o.get("output");
-        if (output instanceof JsonArray jsonArray) {
-            List<JsonElement> outputList = jsonArray.asList();
-            JsonObject block = outputList.get(0).getAsJsonObject();
-            if (id.equals("minecraft:stonecutter_quartz_chiseled") || id.equals("chiseled_quartz_recipeId")
-            ) {
-                block.addProperty("auxValue", 1);
-                outputList.set(0, block);
-            } else if (id.equals("minecraft:stonecutter_quartz_lines") || id.equals("minecraft:pillar_quartz_block")) {
-                block.addProperty("auxValue", 2);
-                outputList.set(0, block);
+    static void fixShapeRecipe(JsonObject org, String block) {
+        f1:
+        for (var b : shapelessCrafting) {
+            String asString = b.get("block").getAsString();
+            JsonObject a1 = b.get("input").getAsJsonObject();
+            JsonObject a2 = org.get("input").getAsJsonObject();
+            JsonArray o1 = b.get("output").getAsJsonArray();
+            JsonArray o2 = org.get("output").getAsJsonArray();
+            if (asString.equals(block) && a1.size() == a2.size() && o1.size() == o2.size()) {
+                if (setShapeLessAux(a1, a2)) {
+                    continue f1;
+                }
+                if (setShapeLessAux(o1, o2)) {
+                    continue f1;
+                }
             }
         }
-        if (Optional.ofNullable(o.get("block")).map(JsonElement::getAsString).orElse("").equals("furnace")) {
-            JsonObject outputF = o.getAsJsonObject("output");
-            String str = o.getAsJsonObject("output").get("id").getAsString();
-            if (str.equals("minecraft:quartz_block")) {
-                outputF.addProperty("auxValue", 3);
+    }
+
+    private static boolean setShapeAux(JsonObject array1, JsonObject array2) {
+        for(var e : array1.entrySet()){
+            for(var e2: array2.entrySet()){
+                String id1 = e.getValue().getAsJsonObject().get("name").getAsString();
+                String id2 = e2.getValue().getAsJsonObject().get("itemId").getAsString();
+                if (id1.equals(id2)){
+                    JsonObject obj1 = e.getValue().getAsJsonObject();
+                    JsonObject obj2 = e2.getValue().getAsJsonObject();
+                    if (obj1.asMap().containsKey("meta")) {
+                        obj2.addProperty("auxValue", asJsonObject1.get("meta").getAsNumber());
+                    }
+                    array2.set(i, asJsonObject);
+                }
+                else {
+
+                }
             }
+        }
+        return false;
+    }
+
+    private static boolean setShapeLessAux(JsonArray array1, JsonArray array2) {
+        for (int i = 0; i < array1.size(); i++) {
+            String id1 = array1.get(i).getAsJsonObject().get("name").getAsString();
+            String id2 = array2.get(i).getAsJsonObject().get("itemId").getAsString();
+            if (!id1.equals(id2)) return true;
+            else {
+                JsonObject asJsonObject = array2.get(i).getAsJsonObject();
+                JsonObject asJsonObject1 = array1.get(i).getAsJsonObject();
+                if (asJsonObject1.asMap().containsKey("meta")) {
+                    asJsonObject.addProperty("auxValue", asJsonObject1.get("meta").getAsNumber());
+                }
+                array2.set(i, asJsonObject);
+            }
+        }
+        return false;
+    }
+
+    static void loadShapelessCraft() {
+        try (var recipe = RecipeFixGen.class.getClassLoader().getResourceAsStream("vanilla_recipes/shapeless_crafting.json")) {
+            assert recipe != null;
+            shapelessCrafting = gson.fromJson(new InputStreamReader(recipe), new TypeToken<List<JsonObject>>() {
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void loadShapeCraft() {
+        try (var recipe = RecipeFixGen.class.getClassLoader().getResourceAsStream("vanilla_recipes/shaped_crafting.json")) {
+            assert recipe != null;
+            shapedCrafting = gson.fromJson(new InputStreamReader(recipe), new TypeToken<List<JsonObject>>() {
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
